2b141a24559269713d5e5e829d90f2ba37c44b18
bakaRs21 <svobodaroman098@gmail.com>
2025-10-14 16:48:51 +0200

    requirements and tailwind to nuxt added

--- frontend/nuxt-app/nuxt.config.ts
+++ frontend/nuxt-app/nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from "nuxt/config"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon'],
+  modules: ['@nuxt/icon', "@nuxtjs/tailwindcss"],
   ssr: true,
   runtimeConfig: {
     public: {
