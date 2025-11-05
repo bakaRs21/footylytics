10bae41996ab442bfbf4dcd41295b9acc4dc4bde
bakaRs21 <svobodaroman098@gmail.com>
2025-11-05 19:58:41 +0100

    prdel ocasovychr nefunguje uz se na to muzu vykakat du na zadek

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,13 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  components: [
+    { 
+      path: "~/components",
+      pathPrefix: false,
+    }
+  ],
   css: ["~/assets/css/tailwind.css"],
   ssr: true,
   runtimeConfig: {
