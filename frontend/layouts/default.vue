991c2ff3ebc0a2323f052cc1351b36f63f176d15
bakaRs21 <svobodaroman098@gmail.com>
2025-11-17 23:07:05 +0100

    concept with default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-wrapper">
+    <Header />
+    <main class="content container">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from '~/components/header.vue'
+import Footer from '~/components/footer.vue'
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,13 +2,9 @@
 </script>
 
 <template>
-    <div class="main-wrapper">
-        <div class="container">
-        <div class="h1-position">
-            <h1 class="h1-design">Premier League</h1>
-        </div>
-        <h3>analyzed data from season 2006 to 2018</h3>
+    <div class="h1-position">
+        <h1 class="h1-design">Premier League</h1>
     </div>
-    </div>
+    <h3>analyzed data from season 2006 to 2018</h3>
     
 </template>
