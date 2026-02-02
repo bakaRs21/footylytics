9df1e4a40970a792abc7d4ad4d12dc48ee5e73d5
bakaRs21 <svobodaroman098@gmail.com>
2026-02-02 23:25:48 +0100

    oujaja funguje

--- backend/Database.py
+++ backend/Database.py
@@ -2,16 +2,21 @@ import dotenv
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker, declarative_base
 from pathlib import Path
-
+import os
 
 base_path = Path(__file__).resolve().parent
 dotenv.load_dotenv(base_path / ".env")
 
-POSTGRES_USER = dotenv.get_key(base_path / ".env", "DB_USER")
+"""POSTGRES_USER = dotenv.get_key(base_path / ".env", "DB_USER")
 POSTGRES_PASSWORD = dotenv.get_key(base_path / ".env", "DB_PASSWORD")
 POSTGRES_HOST = dotenv.get_key(base_path / ".env", "DB_HOST")
 POSTGRES_PORT = dotenv.get_key(base_path / ".env", "DB_PORT")
-POSTGRES_DB = dotenv.get_key(base_path / ".env", "DB_NAME")
+POSTGRES_DB = dotenv.get_key(base_path / ".env", "DB_NAME")"""
+POSTGRES_USER = os.getenv("DB_USER")
+POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
+POSTGRES_HOST = os.getenv("DB_HOST")
+POSTGRES_PORT = os.getenv("DB_PORT")
+POSTGRES_DB = os.getenv("DB_NAME")
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
